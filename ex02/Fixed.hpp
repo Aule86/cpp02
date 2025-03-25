68036df7e547ee43d827610258fe3ac81ec95fb5
Asier Zamorano <asierz86@gmail.com>
2025-03-25 14:45:50 +0100

    ex02

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+
+class Fixed {
+
+	private:
+		int nbr;
+		static const int shift = 8;
+	public:
+		Fixed( );
+		Fixed( const int integer );
+		Fixed( const float floater );
+		Fixed( const Fixed &obj );
+		~Fixed( );
+		Fixed &operator=( const Fixed &obj );
+		Fixed operator+( const Fixed &obj ) const;
+		Fixed operator-( const Fixed &obj ) const;
+		Fixed operator*( const Fixed &obj ) const ;
+		Fixed operator/( const Fixed &obj ) const;
+		bool operator>( const Fixed &obj ) const;
+		bool operator<( const Fixed &obj ) const;
+		bool operator>=( const Fixed &obj ) const;
+		bool operator<=( const Fixed &obj ) const;
+		bool operator==( const Fixed &obj ) const;
+		bool operator!=( const Fixed &obj ) const;
+		Fixed operator++( );
+		Fixed operator++( int );
+		Fixed operator--( );
+		Fixed operator--( int );
+	
+		int getRawBits( void ) const;
+		void setRawBits( const int bits );
+		float toFloat( void ) const;
+		int toInt( void ) const;
+	
+		static Fixed &min(Fixed& f1, Fixed& f2);
+		static Fixed &max(Fixed& f1, Fixed& f2);
+		static Fixed const &min(Fixed const & f1, Fixed const & f2);
+		static Fixed const &max(Fixed const & f1, Fixed const & f2);
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &obj );
